9fd4b844550793c088ce3ba9b2f1f01b3b84168b
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-06-21 18:20:50 -0600

    Search card added

--- starwars.js
+++ starwars.js
@@ -12,13 +12,23 @@ function boton_buscar(){
 
     /*Propiedad de JS para modificar contenido HTML, devuelve una cadena de texto */
     card.innerHTML =
-        `
-        <h2>Nombre del personaje: ${personaje.name}</h2>
-        <p>Año de nacimiento: ${personaje.birth_year}</p>
-        <p>Genero: ${personaje.gender}</p>
-        <p>Altura: ${personaje.height}</p>
-        <p>Color de ojos: ${personaje.eye_color}</p>
-        `
+        
+        `<div class="card text-center">
+            <img src="img/Star-Wars-Logo-Azul.jpg" class="card-img-top" alt="StarWars logo">
+            <div class="card-header text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3">
+                ${personaje.name}
+            </div>
+            <div class="card-body text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+              <p class="card-text">Gender: ${personaje.gender}</p>
+              <p class="card-text">Height: ${personaje.height}</p>
+              <p class="card-text">Mass: ${personaje.mass}</p>
+              <p class="card-text">Hair Color: ${personaje.hair_color}</p>
+              <p class="card-text">Skin Color: ${personaje.skin_color}</p>
+              <p class="card-text">Eye Color: ${personaje.eye_color}</p>
+              <p class="card-text">Birth Year: ${personaje.birth_year}</p>
+            </div>
+        </div>`
+
     
 }
 
@@ -33,3 +43,22 @@ function buscar_personaje(){
 }
 
 
+// `
+//         <h2>Nombre del personaje: ${personaje.name}</h2>
+//         <p>Año de nacimiento: ${personaje.birth_year}</p>
+//         <p>Genero: ${personaje.gender}</p>
+//         <p>Altura: ${personaje.height}</p>
+//         <p>Color de ojos: ${personaje.eye_color}</p>
+//         `
+
+// {"name":"R5-D4",
+// "height":"97",
+// "mass":"32",
+// "hair_color":"n/a",
+// "skin_color":"white, red",
+// "eye_color":"red",
+// "birth_year":"unknown",
+// "gender":"n/a",
+// "homeworld":"https://swapi.dev/api/planets/1/",
+// "films":["https://swapi.dev/api/films/1/"],
+// "species":["https://swapi.dev/api/species/2/"],"vehicles":[],"starships":[],"created":"2014-12-10T15:57:50.959000Z","edited":"2014-12-20T21:17:50.321000Z","url":"https://swapi.dev/api/people/8/"}
